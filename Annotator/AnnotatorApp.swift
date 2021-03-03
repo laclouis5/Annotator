acb632e5a8717d7766f3789c46ce37e5c36d59a0
Lac Louis <lac.louis5@gmail.com>
2021-03-03 18:45:47 +0100

    Initial Commit

---
+++ Annotator/AnnotatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnnotatorApp.swift
+//  Annotator
+//
+//  Created by Louis Lac on 03/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnnotatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Annotator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Annotator
+//
+//  Created by Louis Lac on 03/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
